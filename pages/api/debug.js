9949c2fa2e5ccf96f7b19a7442569ef69f75adbb
LucyAI6 <lucy.fashionai@gmail.com>
2026-03-14 04:57:36 +0100

    Update debug.js

--- pages/api/debug.js
+++ pages/api/debug.js
@@ -1,26 +1,10 @@
 export default async function handler(req, res) {
-try {
-const baseId = process.env.AIRTABLE_BASE_ID;
-const apiKey = process.env.AIRTABLE_API_KEY;
-const tableName = process.env.AIRTABLE_TABLE_NAME;
-
-const url = 'https://api.airtable.com/v0/' + baseId + '/' + encodeURIComponent(tableName);
-
-const response = await fetch(url, {
-headers: { Authorization: 'Bearer ' + apiKey }
-});
-
-const data = await response.json();
-
 res.status(200).json({
-url: url,
-status: response.status,
-erreurAirtable: data.error || null,
-total: data.records?.length || 0,
-champs: data.records?.[0] ? Object.keys(data.records[0].fields) : [],
-premier: data.records?.[0]?.fields || {}
+hasKey: !!process.env.AIRTABLE_API_KEY,
+keyStart: process.env.AIRTABLE_API_KEY?.substring(0, 10) || 'VIDE',
+hasBase: !!process.env.AIRTABLE_BASE_ID,
+baseId: process.env.AIRTABLE_BASE_ID || 'VIDE',
+hasTable: !!process.env.AIRTABLE_TABLE_NAME,
+tableName: process.env.AIRTABLE_TABLE_NAME || 'VIDE',
 });
-} catch (e) {
-res.status(500).json({ erreur: e.message });
-}
 }
